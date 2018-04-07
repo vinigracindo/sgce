aa78510511d9c1e853870e565509417852024729
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2018-04-07 13:09:04 -0300

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sgce.wsgi --log-file -
---
+++ sgce/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sgce project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sgce.settings")
+
+application = Cling(get_wsgi_application())
