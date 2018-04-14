506f33d7622bfbb6c59a55c35eae952e06d89b12
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2018-04-14 13:53:01 -0300

    Cria pasta contrib

---
+++ contrib/secret_gen.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+Django SECRET_KEY generator.
+"""
+from django.utils.crypto import get_random_string
+
+
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+print(get_random_string(50, chars))
