252465f4bfc65344ce085086ff4069c06d19f836
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2018-06-19 17:13:37 -0300

    Adiciona teste no utils

--- sgce/core/apps.py
+++ sgce/core/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    name = 'core'
+# from django.apps import AppConfig
+#
+#
+# class CoreConfig(AppConfig):
+#     name = 'core'
--- sgce/core/tests/test_utils.py
+++ sgce/core/tests/test_utils.py
@@ -18,11 +18,18 @@ class SlugifyTest(TestCase):
         self.assertEqual(self.event.slug, 'simposio-brasileiro-de-informatica')
 
     def test_edit_event(self):
-        """slug should continue the same after editing"""
+        """slug should continue the same if the name doesn't change after editing."""
         self.event.location = 'IFAL - Campus Maceió'
         self.event.save()
         self.event.refresh_from_db()
         self.assertEqual(self.event.slug, 'simposio-brasileiro-de-informatica')
+
+    def test_edit_event_name(self):
+        """slug should change if the name is changed."""
+        self.event.name = 'II Simpósio Brasileiro de Informática'
+        self.event.save()
+        self.event.refresh_from_db()
+        self.assertEqual(self.event.slug, 'ii-simposio-brasileiro-de-informatica')
 
     def test_create_event_same_name(self):
         """slug field should be 'simposio-brasileiro-de-informatica-1'"""
