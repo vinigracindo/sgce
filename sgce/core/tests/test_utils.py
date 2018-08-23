f30945596438d1176fba994de29568f8a0d83bbd
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2018-08-23 18:28:05 -0300

    Remove imports não utilizados

--- sgce/core/tests/test_utils.py
+++ sgce/core/tests/test_utils.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from sgce.core.models import Event
-from sgce.core.utils import slugify
 
 
 class SlugifyTest(TestCase):
