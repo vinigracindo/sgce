791e8292efb302b70a630f713c018b417d7f821f
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2020-11-09 09:38:53 -0300

    closes #13

--- sgce/certificates/views.py
+++ sgce/certificates/views.py
@@ -170,14 +170,14 @@ def template_preview_render_pdf(request, template_pk):
 
     # Create a Django response object, and specify content_type as pdf
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; filename="{}-{}.pdf"'.format(template.name, 'Modelo Teste')
+    response['Content-Disposition'] = 'inline; filename="{}-{}.pdf"'.format(template.id, 'Modelo Teste')
 
     # find the template and render it.
     template = get_template(template_path)
     html = template.render(context)
 
     # create a pdf
-    pisa_status = pisa.CreatePDF(html, dest = response, link_callback = link_callback)
+    pisa_status = pisa.CreatePDF(html, dest=response, link_callback=link_callback)
 
     # if error then show some view
     if pisa_status.err:
@@ -201,7 +201,7 @@ def certificate_render_pdf(request, hash):
     html = template.render(context)
 
     # create a pdf
-    pisa_status = pisa.CreatePDF(html, dest = response, link_callback = link_callback)
+    pisa_status = pisa.CreatePDF(html, dest=response, link_callback=link_callback)
 
     # if error then show some view
     if pisa_status.err:
@@ -222,7 +222,7 @@ def certificates_creator(request):
             template = form.cleaned_data['template']
             certificates = form.cleaned_data['certificates']
 
-            certificates = json.loads(certificates)#[:-1] # Remove last tuple of None's
+            certificates = json.loads(certificates)
 
             inspector = {'certificates': [], 'error': []}
 
@@ -234,12 +234,12 @@ def certificates_creator(request):
                     attrs[template.template_fields()[key]] = value
 
                 participant, created = Participant.objects.get_or_create(
-                    cpf = validate_cpf(cpf),
-                    defaults = {'name': name}
+                    cpf=validate_cpf(cpf),
+                    defaults={'name': name}
                 )
 
                 try:
-                    certificate = Certificate.objects.create(participant = participant, template = template, fields = attrs)
+                    certificate = Certificate.objects.create(participant=participant, template=template, fields=attrs)
                     inspector['certificates'].append(certificate)
                 except IntegrityError:
                     inspector['error'].append(certificate_attrs)
@@ -257,7 +257,7 @@ def certificates_creator(request):
 @login_required
 @certificate_event_created_by_user_logged_in_required
 def certificate_history(request, certificate_pk):
-    certificate = get_object_or_404(Certificate, pk = certificate_pk)
+    certificate = get_object_or_404(Certificate, pk=certificate_pk)
     return render(request, 'certificates/certificate/history.html', {'certificate': certificate})
 
 
@@ -277,7 +277,7 @@ def certificates_evaluation(request):
 @login_required
 @template_event_created_by_user_logged_in_required
 def certificates_evaluation_template(request, template_pk):
-    template = Template.objects.get(pk = template_pk)
+    template = Template.objects.get(pk=template_pk)
 
     context = {'template': template}
 
@@ -288,7 +288,7 @@ def certificates_evaluation_template(request, template_pk):
             status = form.cleaned_data['status']
             certificates = form.cleaned_data['certificates']
 
-            certificates = certificates.exclude(status = status)
+            certificates = certificates.exclude(status=status)
 
             for certificate in certificates:
                 CertificateHistory.objects.create(
@@ -299,9 +299,9 @@ def certificates_evaluation_template(request, template_pk):
                     status = status,
                 )
 
-            certificates.update(status = status)
+            certificates.update(status=status)
 
-            return HttpResponseRedirect(reverse('core:event-detail', args = (template.event.slug,)))
+            return HttpResponseRedirect(reverse('core:event-detail', args=(template.event.slug,)))
     else:
         form = CertificateEvaluationTemplateForm(template_pk)
 
